8de121f834264fb0635afccb73638056d01f2d0c
Junayed6 <abutahirjunayed@gmail.com>
2022-05-03 21:42:24 +0600

    Load data from data base

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
-require('dotenv').config()
+require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 const app = express();
 const port = process.env.PORT || 4000;
@@ -9,15 +10,27 @@ app.use(cors());
 app.use(express.json());
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = `mongodb+srv://${process.env.WAREHOUSE_USER}:${process.env.WAREHOUSE_PASS}@cluster0.v61vj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  console.log('db connected');
-  client.close();
-});
+
+async function run() {
+  try{
+    await client.connect();
+    const furnitureCollection = client.db("warehouse").collection("furniture");
+
+    app.get('/products', async(req,res) => {
+      const query = {};
+      const cursor = furnitureCollection.find(query);
+      const products = await cursor.toArray();
+      res.send(products);
+    })
+  }
+  finally{
+
+  }
+}
+
+run().catch(console.dir);
 
 
 
