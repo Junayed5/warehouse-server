5164589e4c34d7ed69b6641aaa80d43fa0525822
Junayed6 <abutahirjunayed@gmail.com>
2022-05-06 22:33:19 +0600

    Delete confirm

--- index.js
+++ index.js
@@ -40,6 +40,14 @@ async function run() {
       res.send(result);
     })
 
+    //Delete item
+    app.delete('/products/:id', async(req, res) => {
+      const id = req.params.id;
+      const query = {_id:ObjectId(id)};
+      const result = await furnitureCollection.deleteOne(query);
+      res.send(result);
+    })
+
   }
   finally{
 
