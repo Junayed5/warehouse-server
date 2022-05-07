d6c395bb31070c1e19e0fd9aab0ac49364405f25
Junayed6 <abutahirjunayed@gmail.com>
2022-05-07 10:06:48 +0600

    Delivered a sigle item

--- index.js
+++ index.js
@@ -46,6 +46,20 @@ async function run() {
       const query = {_id:ObjectId(id)};
       const result = await furnitureCollection.deleteOne(query);
       res.send(result);
+    });
+
+    app.put('/products/:id', async(req,res) =>{
+      const id = req.params.id;
+      const query = {_id:ObjectId(id)};
+      const body = req.body;
+      const options = { upsert: true };
+      const updateDoc = {
+        $set: {
+          quantity:body.quantity
+        },
+      };
+      const delivered = await furnitureCollection.updateOne(query, updateDoc, options);
+      res.send(delivered);
     })
 
   }
